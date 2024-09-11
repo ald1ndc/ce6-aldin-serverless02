56a13e0d735914813aab46ede373cbd9bba3dffa
ald1ndc <aldindc@yahoo.com>
2024-09-11 14:55:09 +0800

    Updated to add more events and functions

--- SLS2/handler.js
+++ SLS2/handler.js
@@ -1,8 +1,33 @@
 exports.hello = async (event) => {
+  console.log("************logging info - aldin**************");
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
+      message:
+        "Go Serverless v4! Your API function Aldin has executed successfully!",
+    }),
+  };
+};
+
+exports.hello2 = async (event) => {
+  console.log("************logging info 2 - aldin**************");
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message:
+        "Go Serverless v4! Your S3 function Aldin has executed successfully!",
+    }),
+  };
+};
+
+
+exports.hello3 = async (event) => {
+  console.log("************logging info 3 - aldin**************");
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message:
+        "Go Serverless v4! Your SQS function Aldin has executed successfully!",
     }),
   };
 };
